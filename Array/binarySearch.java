83c0759501f290bd633b4ed1b81aa7c62a6a997d
bug-debug-done <ashutoshchoudhary998@gmail.com>
2021-07-05 13:28:31 +0530

    Array

---
+++ Array/binarySearch.java
@@ -0,0 +1,35 @@
+package Array;
+
+import java.util.*;
+  import java.io.*;
+
+  public class binarySearch {
+      public static void main(String args[]) throws IOException {
+          int arr[] = { 5, 7, 10, 12, 15, 20, 27, 31, 36, 42, 55, 58, 60, 65, 70, 80 };
+          System.out.println(binary(arr, 55));
+
+      }
+
+      public static int binary(int a[], int x) {
+
+          int low = 0;
+          int high = a.length - 1;
+
+          while (low <= high) {
+              int mid = (low + high) / 2;
+
+              if (a[mid] < x) {
+                  low = mid + 1;
+              } else if (a[mid] > x) {
+                  high = mid - 1;
+              } else {
+                  return mid;
+              }
+          }
+
+          return -1;
+      }
+
+  }
+
+  //try with 55 and 6
